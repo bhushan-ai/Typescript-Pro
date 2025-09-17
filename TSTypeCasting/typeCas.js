acd6a0fce6f0b55bb3d3b4b47c00fef0d54133b2
bhushan-ai <bhushaningole2004@gmail.com>
2025-09-18 01:01:52 +0530

    type casting in ts

---
+++ TSTypeCasting/typeCas.js
@@ -0,0 +1,13 @@
+//TypeScript  Casting
+//There are times when working with types where it's necessary to override the type of a variable, such as when incorrect types are provided by a library.
+//PROCESS OF OVERRINDING THE TYPES
+var x = "bhushannnn";
+var name1Len = x.toString();
+console.log("whats the name: ", name1Len);
+var user = {
+    name: "bhushann",
+    age: 21,
+    isLoggedIn: true,
+};
+console.log("name of user", user.name);
+console.log("age of user", user.age);
--- TSTypeCasting/typeCas.ts
+++ TSTypeCasting/typeCas.ts
@@ -2,3 +2,33 @@
 
 //There are times when working with types where it's necessary to override the type of a variable, such as when incorrect types are provided by a library.
 
+//PROCESS OF OVERRINDING THE TYPES
+
+let x: unknown = "bhushannnn";
+
+const name1Len = (x as number).toString();
+
+console.log("whats the name: ", name1Len);
+
+//Casting doesn't actually change the type of the data within the variable
+
+//CASTING WITH <>
+
+//Using <> works the same as casting with as.
+
+interface IUser {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
+}
+
+const user = <IUser>{
+  name: "bhushann",
+  age: 21,
+  isLoggedIn: true,
+};
+
+console.log("name of user", user.name);
+console.log("age of user", user.age);
+
+
