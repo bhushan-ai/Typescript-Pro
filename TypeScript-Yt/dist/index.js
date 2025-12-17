22b7f7174dc1b7f80cde60915b2587e4666651d9
bhushan-ai <bhushaningole2004@gmail.com>
2025-12-18 00:53:18 +0530

    new bigining with Ts

---
+++ TypeScript-Yt/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Welcome ${person}`;
+}
+const name = "Bhushannn is smart";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
---
+++ TypeScript-Yt/src/index.ts
@@ -0,0 +1,5 @@
+function greet(person: string): string {
+  return `Welcome ${person}`;
+}
+const name: string = "Bhushannn is smart";
+console.log(greet(name));
